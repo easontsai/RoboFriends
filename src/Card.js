c683f7835f2398b8ec1dbb74ac43c4240e46664f
mteason <eason.tsai@mt.com.tw>
2021-01-31 01:40:53 +0800

    add description of Destructure

--- src/Card.js
+++ src/Card.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Card = (props) => {
   // destructure - 解構賦值
+  // mirrors the construction of array and object literals
   // props.name
   // props.email
   // props.id
+
   const {name, email ,id} = props;
   return(
     <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
